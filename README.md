629cd15c07db8535e5e7044d0bb92f617dcffc62
Aaron Xiao <xiaoxiangquan@gmail.com>
2019-02-15 16:36:12 -0800

    Add filter_keys and substract_keys operator.

--- README.md
+++ README.md
@@ -25,17 +25,26 @@ spark_context = spark_helper.get_context('TestPipeline')
 import pyspark_utils.op as spark_op
 
 # RDD<key, value>  ->  RDD<new_key, value>
-some_pair_rdd.map(spark_op.do_key(lambda key: new_key))
+pair_rdd.map(spark_op.do_key(lambda key: new_key))
 
 # RDD<key, value>  ->  RDD<result>
-some_pair_rdd.map(spark_op.do_tuple(lambda key, value: result))
+pair_rdd.map(spark_op.do_tuple(lambda key, value: result))
 
 # RDD<key, value>  ->  RDD<value, key>
-some_pair_rdd.map(spark_op.swap_kv())
+pair_rdd.map(spark_op.swap_kv())
 
 # RDD<key, value>  ->  RDD<key, value> if func(key)
-some_pair_rdd.filter(spark_op.filter_key(lambda key: true_or_false))
+pair_rdd.filter(spark_op.filter_key(lambda key: true_or_false))
 
 # RDD<key, value>  ->  RDD<key, value> if func(value)
-some_pair_rdd.filter(spark_op.filter_value(lambda value: true_or_false))
+pair_rdd.filter(spark_op.filter_value(lambda value: true_or_false))
+
+# RDD<element>  ->  RDD<element, element>
+rdd.keyBy(spark_op.identity)
+
+# RDD<key, value>   ->   RDD<key, value> with keys in key_rdd
+subset_pair_rdd = spark_op.filter_keys(pair_rdd, key_rdd)
+
+# RDD<key, value>   ->   RDD<key, value> with keys not in key_rdd
+subset_pair_rdd = spark_op.substract_keys(pair_rdd, key_rdd)
 ```
--- pyspark_utils/op.py
+++ pyspark_utils/op.py
@@ -1,13 +1,16 @@
 """The missing PySpark operators."""
 #!/usr/bin/env python
 
+import operator
+
+
 def do_key(func):
     """Return a function which transforms the key."""
     return (lambda kv: (func(kv[0]), kv[1]))
 
 def do_tuple(func):
     """Return a function which transforms a tuple."""
-    return (lambda kv: func(*kv))
+    return (lambda a_tuple: func(*a_tuple))
 
 def filter_key(func):
     """Return a function which filters the key."""
@@ -17,6 +20,21 @@ def filter_value(func):
     """Return a function which filters the value."""
     return (lambda kv: func(kv[1]))
 
-def swap_kv():
-    """An operator to swap a key-value tuple."""
-    return (lambda kv: (kv[1], kv[0]))
+def identity(elem):
+    """Return the argument itself."""
+    return elem
+
+def filter_keys(pair_rdd, key_rdd):
+    """
+    Only keep entries of pair_rdd whose key is in the key_rdd.
+    The given key_rdd should be distinct.
+    """
+    return pair_rdd.join(key_rdd.keyBy(identity)).mapValues(operator.itemgetter(0))
+
+def substract_keys(pair_rdd, key_rdd):
+    """Remove entries of pair_rdd whose key is in the key_rdd."""
+    return pair_rdd.subtractByKey(key_rdd.keyBy(identity))
+
+def swap_kv(kv):
+    """Swap a key-value tuple."""
+    return (kv[1], kv[0])
