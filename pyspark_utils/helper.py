9b6995bef96b5e40d5c9ab93598488c87c4e458e
Aaron Xiao <xiaoxiangquan@gmail.com>
2019-04-27 13:46:44 -0700

    Allow no-element printing.

--- pyspark_utils/helper.py
+++ pyspark_utils/helper.py
@@ -4,14 +4,17 @@
 import colored_glog as glog
 
 
-def cache_and_log(rdd_name, rdd, items=1,
-                  info_func=glog.info, warn_func=glog.warn):
+def cache_and_log(rdd_name, rdd, show_items=1, info_func=glog.info, warn_func=glog.warn):
     """Cache and pretty log an RDD, then return the rdd itself."""
     rdd = rdd.cache()
-    elem_count = rdd.count()
-    if elem_count > 0:
-        info_func('{} has {} elements: [{}, ...]'.format(
-            rdd_name, elem_count, rdd.take(items)))
-    else:
+    count = rdd.count()
+    if count == 0:
         warn_func('{} is empty!'.format(rdd_name))
+    elif show_items == 0:
+        info_func('{} has {} elements.'.format(rdd_name, count))
+    elif show_items == 1:
+        info_func('{} has {} elements: [{}, ...]'.format(rdd_name, count, rdd.first()))
+    else:
+        info_func('{} has {} elements: [{}, ...]'.format(rdd_name, count,
+                                                         ', '.join(rdd.take(show_items))))
     return rdd
